d49d8ebb1ac0dfe97f2a67adcc72ca4c192a83aa
zrb <1923499469@qq.com>
2024-06-17 22:36:05 +0800

    second commit 基于泛型+函数式编程实现了缓存穿透和缓存击穿的方法

--- src/main/java/com/zrb41/utils/RedisConstants.java
+++ src/main/java/com/zrb41/utils/RedisConstants.java
@@ -6,6 +6,16 @@ public class RedisConstants {
     // 验证码有效期
     public static final Long LOGIN_CODE_TTL=10l;
 
+    // token的key
     public static final String LOGIN_TOKEN="login:token:";
+    // token的有效期
     public static final Long LOGIN_TOKEN_TTL=30l;
+
+    // 球员信息的key
+    public static final String CACHE_PLAYER="cache:playerId:";
+    // 球员信息的key的有效期
+    public static final Long CACHE_PLAYER_TTL=30l;
+
+    // 球员锁
+    public static final String LOCK_PLAYER_KEY="lock:player:";
 }
